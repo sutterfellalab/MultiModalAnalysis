27032a6dbc626eb2897b16c7a385b147db54258b
roncofaber <fabrice.roncoroni@gmail.com>
2024-11-29 18:20:19 -0800

    fixed command line tool

--- mmanalysis/cli/mmanalysis_cli.py
+++ mmanalysis/cli/mmanalysis_cli.py
@@ -13,11 +13,13 @@ import mmanalysis.main_analysis
 
 #%%
 
-if __name__ == "__main__":
-    
+def main():
     parser = argparse.ArgumentParser(description="Run MMAnalysis with specified parameters.")
     parser.add_argument('-f', '--folder', type=str, default=None, help="Path to the folder to analyze.")
     
     args = parser.parse_args()
 
     mmanalysis.main_analysis.main(folder=args.folder)
+
+if __name__ == "__main__":
+    main()
