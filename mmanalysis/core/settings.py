98808f655b95862f410a72325d71fed866d22ef9
TimKodalle <74673834+TimKo90@users.noreply.github.com>
2025-06-16 13:58:41 -0700

    Prep for pyFAI-integration - testing only
    Hard-coded locations, needs updating before merge

--- mmanalysis/core/settings.py
+++ mmanalysis/core/settings.py
@@ -4,33 +4,48 @@ Created on Thu Dec 22 11:56:04 2022
 
 @author: Tim Kodalle
 """
-
-#%%PL-Settings:
     
 def generalParameters():
     
     genParams = {
                     'GIWAXS' : True,
-                    'PL' : True,
+                    'PL' : False,
                     'Logging': True,
-                    'TempOld' : False,
                     
-                    'LabviewPL'  : True,     # BL PL via Labview
+                    'ImagePlots' : 'Node-centered',         # Igor Pro works node-centered, i.e. the x- and y- arrays mark the nodes or corners of each pixel in the z-data matrix
+                    # 'ImagePlots' : 'Pixel-centered        # Origin works pixel-centered, i.e. the x- and y- arrays mark the center of each pixel in the z-data matrix
                     }
     
     return genParams
+
+def giwaxsParameters():
+    
+    giwaxsParams = {
+                    'GIWAXS-calibration': 'C:/Users/timko/OneDrive/Documents/ActiveProjects/Maged/Maged_Dec24/Maged/h5files/default_calibration.poni', 
+                    # 'GIWAXS-calibration': None,
+                    
+                    'calibrant' : 'G:/My Drive/Code/Multimodal_Analysis/mMA-Script_h5/files/alumina.D',
+                    'calibration-image' : 'G:/My Drive/Code/Multimodal_Analysis/mMA-Script_h5/files/Al2O3_calib_10keV_6p25_2p0_35p0_10s.tif',
+                    'energy' : '10',
+                    'default-poni' : "G:/My Drive/Code/Multimodal_Analysis/mMA-Script_h5/files/default_calibration.poni",
+                    'Re-calibrant' : 'ITO',
+                    'ITO-calibrant' : "G:/My Drive/Code/Multimodal_Analysis/mMA-Script_h5/files/ito_calibrant.D",
+                    'ai_npts' : 1000,
+                    'ai_range': [160, 180], ## Check after image rotation if this is still correct, consider larger range and/or mirroring it
+                    }
+    
+    return giwaxsParams
     
 def plParameters():
     
     plParams = {    
                    'Thorlabs' : False,    # If Thorlabs software is used instead of OceanView
                    'smoothing' : False,   # Smoothing of the data to reduce noise
-                   'Labview'  : True,     # BL PL via Labview
                    'sFactor' : 3,         # Parameter for smoothing with a SavGol-Filter
                    'bkgCorr' : False,     # Enable linear background removal. If True, the program will ask for two ranges for the removal. I recommend setting one of them at higher and the other at lower energy compared to the peaks of interest.
                    'bkgCorrPoly' : 1,     # This parameter determines the order of the polynomial fit used for background correction (0=const, 1=linear, etc.)
                    'binning' : 0,         # 0: no binning, >0: Binning of n spectra into one, i.e. reducing the time resolution for increased signal to noise ratio
-                   'logplots': 0,
+                   'logplots': False,     # Set to 'True' if you want the script to display the PL-maps with log(intensity)
                    }
         
     return plParams
