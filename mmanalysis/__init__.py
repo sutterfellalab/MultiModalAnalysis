603b4b0395564ad1b946c18ea423eaebb675c03d
TimKo90 <tim.kodalle@gmail.com>
2026-09-01 11:39:30 -0700

    Bump version to 1.0.4

--- mmanalysis/__init__.py
+++ mmanalysis/__init__.py
@@ -7,6 +7,6 @@ Created on Tue Dec 20 11:12:01 2022
 """
 
 # package info
-__version__ = '1.0.2'
-__date__    = "17 Jul. 2025"
+__version__ = '1.0.4'
+__date__    = "01 Sep. 2026"
 __author__  = "sutterfellalab"
